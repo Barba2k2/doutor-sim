7ee7236a1265a789cfdc62a51c2c718718801247
Barba2k2 <lorenzo.dz@hotmail.com>
2024-02-06 11:38:16 -0300

    chore: add ant design set-up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { AntDesignProvider } from "./providers/AntDesign";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AntDesignProvider>{children}</AntDesignProvider>
+      </body>
     </html>
   );
 }
